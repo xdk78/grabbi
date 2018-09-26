cb34dfb858805df15511f48ae60504ffa1ecdb8e
xdk78 <xdk78888@gmail.com>
2018-09-26 20:28:04 +0200

    fix: ts typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,3 @@
 import { AxiosRequestConfig, AxiosResponse } from 'axios';
-declare type grabbi = (url: string, opts?: AxiosRequestConfig) => Promise<{ doc: HTMLDocument, res: AxiosResponse }>;
+declare const grabbi: (url: string, opts?: AxiosRequestConfig) => Promise<{ doc: HTMLDocument, res: AxiosResponse }>;
 export default grabbi;
